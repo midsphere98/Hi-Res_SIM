e419595dc659b3eded1547374e4dbc4c8046c6b4
TSOI <62754976+tsoinikolas@users.noreply.github.com>
2022-02-06 00:47:00 +0900

    Update 1.1.ino
    RTC 살려냈음

--- 1.1.ino
+++ 1.1.ino
@@ -1,74 +1,61 @@
-#include <DS1302.h>
 #include <stdio.h>
-//stepper input
-int PUL=7; //define Pulse pin
-int DIR=6; //define Direction pin
-int ENA=5; //define Enable Pin
-
-//RCT input
-const int RST = 2; 
-const int IO = 1; //dat
-const int CLK = 0;
-
-//creating rct obj
-DS1302 rtc(RST, IO, CLK);
-
-String dayAsString(const Time::Day day) {
-  switch (day) {
-    case Time::kSunday: return "Sunday";
-    case Time::kMonday: return "Monday";
-    case Time::kTuesday: return "Tuesday";
-    case Time::kWednesday: return "Wednesday";
-    case Time::kThursday: return "Thursday";
-    case Time::kFriday: return "Friday";
-    case Time::kSaturday: return "Saturday";
-  }
-  return "(unknown day)";
-}
+#include <DS1302.h>
 
 
-void setup() 
-{
-  pinMode (PUL, OUTPUT);
-  pinMode (DIR, OUTPUT);
-  pinMode (ENA, OUTPUT);
-  Serial.begin(9600);
+int Green = 5;
+unsigned long past = 0;
+int buzzerPin = 6;
+int flag = 0; // 과거 기준 시간보다 1000ms 지났음을 판단하는 플래그. 시간 플로팅에 이용됨.
+
+DS1302 rtc(4, 3, 2);
+
+void setup() {
+  //RTC를 시작하고 작성보호모드(Write-Protection)을 해제 
   rtc.halt(false);
   rtc.writeProtect(false);
-  Time t(2022, 1, 27, 12, 52, 50, Time::kSunday);
-  rtc.time(t);
+  //시리얼 시작
+  Serial.begin(9600);
+  
+  //시간,날짜 초기 설정. 설정 이후 주석 처리 하여 리셋되지 않도록 하여야
+  //rtc.setDOW(SATURDAY);
+  rtc.setTime(0, 43, 40); //시간 포맷은 24hr임
+  //rtc.setDate(5, 2, 2022);
+  pinMode(Green, OUTPUT);
 }
 
-//아워미터
-void printTime() {
-  // Get the current time and date from the chip.
-  Time t = rtc.time();
-
-  // Name the day of the week.
-  const String day = dayAsString(t.day);
-
-  // Format the time and date and insert into the temporary buffer.
-  char buf[50];
-  snprintf(buf, sizeof(buf), "%s %04d-%02d-%02d %02d:%02d:%02d",
-           day.c_str(),
-           t.yr, t.mon, t.date,
-           t.hr, t.min, t.sec);
-
-  // Print the formatted string to serial so we can see the time.
-  Serial.println(buf);
+void time()
+{
+  //요일 출력
+  Serial.print(rtc.getDOWStr());
+  Serial.print(" ");
+  //날짜 출력
+  Serial.print(rtc.getDateStr());
+  Serial.print(" -- ");
+  //시간 출력
+  Serial.print(rtc.getTimeStr());
+  Serial.print("\n");
 }
 
+void LED()
+{
+  unsigned long times = millis();
+  //1초 대기 후 점
+  if(times - past >= 1000)
+  {
+    past = times;
+    flag = 1;
+  }
+
+  if(flag ==1)
+  {
+    digitalWrite(Green, !(digitalRead(Green)));
+    time();
+    flag = 0;
+  }
+
+}
 
 void loop() {
-  for (int i=0; i<4000; i++)   //i 값이 클 수록 많이 내려가고 작을 수록 조금 내려감 
-  {
-    digitalWrite(DIR,HIGH); //LOW면 내려가고 HIGH면 올라감
-    digitalWrite(ENA,HIGH);
-    digitalWrite(PUL,HIGH);
-    delayMicroseconds(800); //속도조절 -> 값이 낮을 수록 빠름
-    digitalWrite(PUL,LOW);
-    delayMicroseconds(800); //속도조절 -> 값이 낮을 수록 빠름
-  }
-  printTime();
-  delay(0); //i만큼 내려간 이후 얼마나 기다리느냐 : 1000이 1초
+  LED();
+
 }
